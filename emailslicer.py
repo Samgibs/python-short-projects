f48c534f45d57ef9e2fef163cb5897aba64b63f7
Samgibs <captainsamuelqs@gmail.com>
2023-10-11 14:30:11 +0300

    new-email-slicer

--- emailslicer.py
+++ emailslicer.py
@@ -1,14 +1,13 @@
-def main():
-    print("welcome to the email slicer!")
+def emailslicer():
+    print("welcome to email slicer")
     print("")
 
-    email_input=input("input your emailaddress: ")
+    email_input=input("enter your email address: ")
     (username,domain)=email_input.split("@")
     (domain,extension)=domain.split(".")
 
-    print("username :   ",username)
-    print("domain:  ",domain)
+    print("username:    ",username)
+    print("domain:   ",domain)
     print("extension:   ",extension)
 
-
-main()
+emailslicer()
